37a93506e666aaa7ed0920229c63819649e2c402
Samuel Juhel <samuel.juhel@zaclys.net>
2023-05-02 17:35:42 +0200

    should look at code more closely

--- boario_tools/floods.py
+++ boario_tools/floods.py
@@ -139,7 +139,7 @@ def get_events_in_MRIO_regions(df, mrios_shapes, mrio_name):
         mrio_shapes_df, how="left", max_distance=30410, distance_col="distance"
     )
     # Following https://stackoverflow.com/a/43855963/4703808
-    gdf_tmp = gdf.loc[gdf_tmp.astype(str).drop_duplicates().index]
+    gdf_tmp = gdf_tmp.loc[gdf_tmp.astype(str).drop_duplicates().index]
     scriptLogger.info("......Done, merging and returning")
     gdf_eu = pd.concat([gdf_eu, gdf_tmp], axis=0)
     cols_select = df.columns.drop("geometry").union(pd.Index(["mrio_region", "mrio"]))
